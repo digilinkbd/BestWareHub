d7e942281440786ec6a8f2cbaf8a38df2fec90cb
digilinkbd <digilinkbd@gmail.com>
2025-05-04 18:53:23 +0600

    Product SEO

--- app/(home)/p/[slug]/page.tsx
+++ app/(home)/p/[slug]/page.tsx
@@ -1,6 +1,29 @@
+
+import { getProductBySlug } from '@/actions/products';
 import ProductPage from '@/components/frontend/product-page';
 import React from 'react'
 
+//app/products/[slug]
+export async function generateMetadata({ params: { slug },
+  }: {
+    params: { slug: string };
+  }
+) {
+  const product = await getProductBySlug(slug);
+  //const { product } = await import('@/hooks/useProduct').then((mod) => mod.useProductDetails(slug))
+  return {
+    title: product?.title,
+    description: product?.description,
+    alternates: {
+      canonical: `/products/${product?.slug}`,
+    },
+    openGraph: {
+      title: product?.title,
+      description: product?.description,
+      images: [product?.productImages],
+    },
+  };
+}
 export default async function page({
   params,
 }: {
